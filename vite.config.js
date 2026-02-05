37f6db3921d075db694f8fb04471d5e9eb64d14a
3030664772@qq.com <3030664772@qq.com>
2026-02-05 14:48:48 +0800

    5

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  base: './',
   build:{
     outDir: 'dist',
     assetsDir: 'assets'
