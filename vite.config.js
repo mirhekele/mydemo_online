5dd755442772ef24d109a739ddbe215f035a9dfb
3030664772@qq.com <3030664772@qq.com>
2026-02-05 14:26:42 +0800

    2

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV === 'production' ? '/demoonline/' : '/',
+  build:{
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
